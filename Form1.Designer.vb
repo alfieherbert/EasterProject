aadf05311cb7acfcac260666550e3b48a4d81357
Alfie <alfieherbert@duck.com>
2023-03-30 22:43:21 +0100

    Add save button and input boxes

---
+++ Form1.Designer.vb
@@ -0,0 +1,78 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        NameBox = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox5 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' NameBox
+        ' 
+        NameBox.Location = New Point(108, 212)
+        NameBox.Name = "NameBox"
+        NameBox.Size = New Size(125, 27)
+        NameBox.TabIndex = 0
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(266, 212)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(125, 27)
+        TextBox2.TabIndex = 1
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(434, 212)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(125, 27)
+        TextBox3.TabIndex = 2
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(580, 212)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(125, 27)
+        TextBox5.TabIndex = 4
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(TextBox5)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(NameBox)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents NameBox As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+
+    Private Sub NameBox_TextChanged(sender As Object, e As EventArgs) Handles NameBox.TextChanged
+        MsgBox(e.GetType.ToString)
+    End Sub
+End Class
+
+
